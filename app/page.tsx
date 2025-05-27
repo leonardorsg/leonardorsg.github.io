9aa9fb2b05d64ba8a86709acaf4ccd52857c45b2
Leonardo Garcia <leonardorsg@hotmail.com>
2025-05-27 17:27:01 +0100

    isolated

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 
 import { navItems } from "@/data";
 
-import Hero from "@/components/Hero";
+//import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
+//import Footer from "@/components/Footer";
+//import Approach from "@/components/Approach";
+//import Experience from "@/components/Experience";
+//import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
@@ -15,12 +15,8 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />
         <Grid />
-        <RecentProjects />
-        <Experience />
-        <Approach />
-        <Footer />
+
       </div>
     </main>
   );
