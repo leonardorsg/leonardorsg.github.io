f52ebb270d116b779378110846c4b27870af75c9
Leonardo Garcia <leonardorsg@hotmail.com>
2025-05-27 17:22:49 +0100

    use client

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
@@ -31,16 +33,14 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        
-
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <div
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <a href={info.link} >
-              <img src={info.img} alt="icons" width={20} height={20} />
+              <a href={info.link}>
+                <img src={info.img} alt="icons" width={20} height={20} />
               </a>
             </div>
           ))}
