e2612fe3e5a6c86e4289f4eb92a875ad945418d1
Leonardo Garcia <leonardorsg@hotmail.com>
2025-05-31 16:06:06 +0100

    change build

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: 'https://leonardorsg.github.io/',
+    images: {
+        unoptimized: true,
+    },
     typescript: {
         ignoreBuildErrors: true, // Ignore TypeScript errors during build
     },
+
+
 };
+module.exports = nextConfig;
